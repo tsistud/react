f12bfa765d941c9a3485ac593b7e44017f2329d1
tsistud <isakov_d@auca.kg>
2024-11-14 12:56:25 +0600

    product-card-css

--- src/components/Productcard/index.jsx
+++ src/components/Productcard/index.jsx
@@ -1,7 +1,14 @@
-function Productcard(){
+import "./index.css";
+
+export function Productcard(props){
+    const {img, category, name, price}= props.data;
+
     return(
-        <div>Productcard</div>
+        <div className='product-card'>
+            <img width="200" src={img} alt="" />
+            <div>{category}</div>
+            <h4>{name}</h4>
+            <div>{price}</div>
+        </div>
     )
 }
-
-export default Productcard;
